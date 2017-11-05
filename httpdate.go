88cd849178ec676982026d86dde15a41c9867ec6
Songmu <y.songmu@gmail.com>
2017-11-05 16:22:01 +0900

    port most format reg

--- httpdate.go
+++ httpdate.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
 var (
 	// Thu, 03 Feb 1994 00:00:00 GMT
-	fastReg = regexp.MustCompile(`^[SMTWF][a-z][a-z], (\d\d) ([JFMAJSOND][a-z][a-z]) (\d\d\d\d) (\d\d):(\d\d):(\d\d) GMT$`)
+	fastReg        = regexp.MustCompile(`^[SMTWF][a-z][a-z], (\d\d) ([JFMAJSOND][a-z][a-z]) (\d\d\d\d) (\d\d):(\d\d):(\d\d) GMT$`)
+	uselessWdayReg = regexp.MustCompile(`^(?i)(?:Sun|Mon|Tue|Wed|Thu|Fri|Sat)[a-z]*,?\s*`)
+	mostFormatReg  = regexp.MustCompile(`^(\d\d?)` + // day
+		`(?:\s+|[-\/])` +
+		`(\w+)` + // month
+		`(?:\s+|[-\/])` +
+		`(\d+)` + // year
+		`(?:` +
+		`(?:\s+|:)` + // separator before clock
+		`(\d\d?):(\d\d)` + // hour:min
+		`(?::(\d\d))?` + // optional seconds
+		`)?` + // optional clock
+		`\s*` +
+		`([-+]?\d{2,4}|[A-Za-z]+)?` + // timezone
+		`\s*` +
+		`(?:\(\w+\)|\w{3,})?` + // ASCII representation of timezone.
+		`\s*$`)
 )
 
 var shortMonth2Month = map[string]time.Month{
@@ -45,5 +62,8 @@ func Str2Time(timeStr string, loc *time.Location) (time.Time, error) {
 			loc,
 		), nil
 	}
+	timeStr = strings.TrimSpace(timeStr)
+	timeStr = uselessWdayReg.ReplaceAllString(timeStr, "")
+
 	return time.Time{}, fmt.Errorf("not implemented")
 }
