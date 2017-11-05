2a6332d218990e422dcedb94fbe4b633d9f3c1e2
Songmu <y.songmu@gmail.com>
2017-11-05 17:34:28 +0900

    implement most formats

--- httpdate.go
+++ httpdate.go
@@ -12,20 +12,20 @@ var (
 	// Thu, 03 Feb 1994 00:00:00 GMT
 	fastReg        = regexp.MustCompile(`^[SMTWF][a-z][a-z], (\d\d) ([JFMAJSOND][a-z][a-z]) (\d\d\d\d) (\d\d):(\d\d):(\d\d) GMT$`)
 	uselessWdayReg = regexp.MustCompile(`^(?i)(?:Sun|Mon|Tue|Wed|Thu|Fri|Sat)[a-z]*,?\s*`)
-	mostFormatReg  = regexp.MustCompile(`^(\d\d?)` + // day
+	mostFormatReg  = regexp.MustCompile(`^(\d\d?)` + // 1. day
 		`(?:\s+|[-\/])` +
-		`(\w+)` + // month
+		`(\w+)` + // 2. month
 		`(?:\s+|[-\/])` +
-		`(\d+)` + // year
+		`(\d+)` + // 3. year
 		`(?:` +
 		`(?:\s+|:)` + // separator before clock
-		`(\d\d?):(\d\d)` + // hour:min
-		`(?::(\d\d))?` + // optional seconds
+		`(\d\d?):(\d\d)` + // 4. 5. hour:min
+		`(?::(\d\d))?` + // 6. optional seconds
 		`)?` + // optional clock
 		`\s*` +
-		`([-+]?\d{2,4})?` + // offset
+		`([-+]?\d{2,4})?` + // 7. offset
 		`\s*` +
-		`(\w+)?` + // ASCII representation of timezone.
+		`(\w+)?` + // 8. ASCII representation of timezone.
 		`\s*$`)
 )
 
@@ -49,9 +49,24 @@ func a2i(str string) int {
 	return i
 }
 
+var fourDigitsReg = regexp.MustCompile(`^([-+])?(\d\d?):?(\d\d)?$`)
+
+func fourDigits2offset(str string) int {
+	if matches := fourDigitsReg.FindStringSubmatch(str); len(matches) > 0 {
+		hour := a2i(matches[2])
+		min := a2i(matches[3])
+		offset := hour*60*60 + min*60
+		if matches[1] == "-" {
+			offset *= -1
+		}
+		return offset
+	}
+	return 0
+}
+
 func Str2Time(timeStr string, loc *time.Location) (time.Time, error) {
 	if matches := fastReg.FindStringSubmatch(timeStr); len(matches) > 0 {
-		return time.Date(
+		d := time.Date(
 			a2i(matches[3]),
 			shortMonth2Month[matches[2]],
 			a2i(matches[1]),
@@ -59,11 +74,56 @@ func Str2Time(timeStr string, loc *time.Location) (time.Time, error) {
 			a2i(matches[5]),
 			a2i(matches[6]),
 			0,
-			loc,
-		), nil
+			time.UTC,
+		)
+		if loc != nil {
+			d = d.In(loc)
+		}
+		return d, nil
 	}
 	timeStr = strings.TrimSpace(timeStr)
 	timeStr = uselessWdayReg.ReplaceAllString(timeStr, "")
+
+	if matches := mostFormatReg.FindStringSubmatch(timeStr); len(matches) > 0 {
+		maybeAMPM := strings.ToLower(matches[8])
+		if maybeAMPM != "am" || maybeAMPM != "pm" {
+			var l *time.Location
+			if matches[8] != "" {
+				l2, err := time.LoadLocation(matches[8])
+				if err == nil {
+					l = l2
+				}
+			}
+			if l == nil && matches[7] != "" {
+				l = time.FixedZone(matches[8], fourDigits2offset(matches[7]))
+			}
+			if l == nil {
+				l = loc
+				if l == nil {
+					l = time.UTC
+				}
+			}
+
+			y := a2i(matches[3])
+			if y < 100 {
+				y += 1900
+			}
+			d := time.Date(
+				y,
+				shortMonth2Month[matches[2]],
+				a2i(matches[1]),
+				a2i(matches[4]),
+				a2i(matches[5]),
+				a2i(matches[6]),
+				0,
+				l,
+			)
+			if loc != nil {
+				d = d.In(loc)
+			}
+			return d, nil
+		}
+	}
 
 	return time.Time{}, fmt.Errorf("not implemented")
 }
--- httpdate_test.go
+++ httpdate_test.go
@@ -16,6 +16,46 @@ func TestStr2Time(t *testing.T) {
 			name:  "RFC1123",
 			input: "Thu, 03 Feb 1994 00:00:00 GMT",
 		},
+		{
+			name:  "old rfc850 HTTP format",
+			input: "Thursday, 03-Feb-94 00:00:00 GMT",
+		},
+		{
+			name:  "broken rfc850 HTTP format",
+			input: "Thursday, 03-Feb-1994 00:00:00 GMT",
+		},
+		{
+			name:  "common logfile format1",
+			input: "03/Feb/1994:00:00:00 0000",
+		},
+		{
+			name:  "common logfile format2",
+			input: "03/Feb/1994:01:00:00 +0100",
+		},
+		{
+			name:  "common logfile format1",
+			input: "02/Feb/1994:23:00:00 -0100",
+		},
+		{
+			name:  "HTTP format (no weekday)",
+			input: "03 Feb 1994 00:00:00 GMT",
+		},
+		{
+			name:  "old rfc850 (no weekday)",
+			input: "03-Feb-94 00:00:00 GMT",
+		},
+		{
+			name:  "broken rfc850 (no weekday)",
+			input: "03-Feb-1994 00:00:00 GMT",
+		},
+		{
+			name:  "broken rfc850 (no weekday, no seconds)",
+			input: "03-Feb-1994 00:00 GMT",
+		},
+		{
+			name:  "VMS dir listing format",
+			input: "03-Feb-1994 00:00",
+		},
 	}
 	for _, tc := range testCases {
 		out, err := Str2Time(tc.input, time.UTC)
